b893f743c2c8379c4ca1451befd3697ce3d56f8c
monisharajan <monisha.rajan@thoughtworks.com>
2023-05-14 11:37:45 +0530

    Refactor EntryLine component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import { Container, Grid, Icon, Segment } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import './App.css';
 import DisplayBalance from './components/DisplayBalance';
+import DisplayBalances from './components/DisplayBalances';
+import EntryLine from './components/EntryLine';
 import MainHeader from './components/MainHeader';
 import NewEntryForm from './components/NewEntryForm';
 
@@ -11,61 +13,16 @@ function App() {
       <MainHeader title = "Budget"></MainHeader>
       <DisplayBalance title="Your Balance:" value="2,550.90" size="small" />
 
-      <Segment textAlign='center'>
-        <Grid columns={2} divided>
-          <Grid.Row>
-            <Grid.Column>
-             <DisplayBalance title="Income:" value="1234.50" color="green" />
-            </Grid.Column>
-            <Grid.Column>
-              <DisplayBalance title="Expenses:" value="700.50" color="red" />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
-
+      <DisplayBalances />
       <MainHeader title="History" type ="h3" />
-      <Segment color='red'>
-        <Grid columns={3} textAlign='right'>
-          <Grid.Row>
-            <Grid.Column width={10} textAlign='left'>text1</Grid.Column>
-            <Grid.Column width={3} textAlign='right'>$10.00</Grid.Column>
-            <Grid.Column width={3}>
-              <Icon name='edit' bordered />
-              <Icon name='trash' />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
-
-      <Segment color='green'>
-        <Grid columns={3} textAlign='right'>
-          <Grid.Row>
-            <Grid.Column width={10} textAlign='left'>text2</Grid.Column>
-            <Grid.Column width={3} textAlign='right'>$20.00</Grid.Column>
-            <Grid.Column width={3}>
-              <Icon name='edit' bordered />
-              <Icon name='trash' />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
-
-      <Segment color='red'>
-        <Grid columns={3} textAlign='right'>
-          <Grid.Row>
-            <Grid.Column width={10} textAlign='left'>text1</Grid.Column>
-            <Grid.Column width={3} textAlign='right'>$10.00</Grid.Column>
-            <Grid.Column width={3}>
-              <Icon name='edit' bordered />
-              <Icon name='trash' />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
+     
+     <EntryLine description="Income1" value="$100.00" isExpense/>
+     <EntryLine description="Expense1" value="$20.00" />
+     <EntryLine description="Income2" value="$30.00" isExpense/>
 
       <MainHeader title="Add new transaction" type ="h3" />
       <NewEntryForm />
+      
     </Container>
   );
 }
